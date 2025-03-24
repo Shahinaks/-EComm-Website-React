65478e5f748cf6e106075e7f91e7316b0329e226
Shahina <shahina.sharafudeenki@gmail.com>
2025-03-24 22:59:35 +0530

    Deleted Test.jsx

--- src/Components/Test.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const Test = () => {
-  const { id } = useParams();
-  console.log('Product ID:', id);
-  return <div>Product ID: {id}</div>;
-};
-
-export default Test;
